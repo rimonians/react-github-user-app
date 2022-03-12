8ef3d3bc9241acb41a86db3f64ba39e322d328e5
Rimon Ahmed <rimonians@gmail.com>
2022-03-12 21:58:37 +0600

    Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className={classes.navbar}>
-      <Link to="/">Github Users App</Link>
+      <Link to="/react-github-user-app">Github Users App</Link>
     </div>
   );
 };
